c754e3bc1214a629ea36a104a68954415a1e7d8c
Hear-me2021 <80481577+Hear-me2021@users.noreply.github.com>
2021-08-13 11:27:41 +0200

    Update settings
    I changed the names of some settings.

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import {View, Text, StyleSheet, SectionList, Image} from 'react-native';
+import { View, StyleSheet, SectionList } from 'react-native';
+//import IconAntDesign from 'react-native-vector-icons/AntDesign';
+//import {Icon} from 'react-native-elements'
 
-import {
-  ListItem,
-  Divider,
-  SearchBar,
-  Header,
-  Icon,
-} from 'react-native-elements';
+import { ListItem, Divider, SearchBar, Header, Icon  } from 'react-native-elements';
 
 const ORANGE = '#FF9500';
 const BLUE = '#007AFF';
@@ -18,218 +14,202 @@ const PURPLE = '#5856D6';
 const TEAL_BLUE = '#5AC8FA';
 
 const sections = [
-  {
-    data: [
-      {
-        title: 'Airplane Mode',
-        icon: 'ios-airplane',
-        backgroundColor: ORANGE,
-        hideChevron: true,
-        checkbox: true,
-      },
-      {
-        title: 'Wi-Fi',
-        backgroundColor: BLUE,
-        icon: 'ios-wifi',
-      },
-      {
-        title: 'Bluetooth',
-        backgroundColor: BLUE,
-        icon: 'ios-bluetooth',
-        //rightTitle: 'Off',
-        hideChevron: true,
-        checkbox: true,
-      },
-      {
-        title: 'Cellular',
-        backgroundColor: GREEN,
-        icon: 'ios-phone-portrait',
-      },
-      {
-        title: 'Personal Hotspot',
-        backgroundColor: GREEN,
-        icon: 'ios-radio',
-        rightTitle: 'Off',
-      },
-    ],
-  },
-  {
-    data: [
-      {
-        title: 'Notifications',
-        icon: 'ios-notifications',
-        backgroundColor: RED,
-      },
-      {
-        title: 'Control Center',
-        backgroundColor: GREY,
-        icon: 'ios-switch',
-      },
-      {
-        title: 'Do Not Disturb',
-        backgroundColor: PURPLE,
-        icon: 'ios-moon',
-      },
-    ],
-  },
-  {
-    data: [
-      {
-        title: 'General',
-        icon: 'ios-settings',
-        backgroundColor: GREY,
-      },
-      {
-        title: 'Display & Brightness',
-        backgroundColor: BLUE,
-        icon: 'ios-bulb',
-      },
-      {
-        title: 'Wallpaper',
-        backgroundColor: TEAL_BLUE,
-        icon: 'ios-color-wand',
-      },
-      {
-        title: 'Sounds',
-        backgroundColor: RED,
-        icon: 'ios-volume-high',
-      },
-      {
-        title: 'Touch ID & Code',
-        backgroundColor: RED,
-        icon: 'ios-finger-print',
-      },
-      {
-        title: 'Emergency Call',
-        backgroundColor: ORANGE,
-        icon: 'ios-medical',
-      },
-      {
-        title: 'Battery',
-        backgroundColor: GREEN,
-        icon: 'ios-battery-full',
-      },
-      {
-        title: 'Confidentiality',
-        backgroundColor: GREY,
-        icon: 'ios-hand',
-      },
-    ],
-  },
-  // Space at the bottom
-  {data: []},
+    {
+        
+        data: [
+
+            {
+                title: '   Mode',
+                icon: 'sound',
+                backgroundColor: ORANGE,
+               
+                
+            },
+               
+            {
+                title: '   Neckband',
+                icon: 'ios-bluetooth',
+                backgroundColor: BLUE,
+                hideChevron: true,
+                checkbox: true,
+            },
+            {
+                title: '   Hearing aids',
+                backgroundColor: BLUE,
+                icon: 'ios-bluetooth',
+                hideChevron: true,
+                checkbox: true,
+            },
+            {
+                title: '   Vibrations',
+                backgroundColor: GREEN,
+                icon: 'ios-radio',
+                //rightTitle: 'Off',
+                hideChevron: true,
+                checkbox: true,
+            },
+            {
+                title: '   Vibration intensity',
+                backgroundColor: GREEN,
+                icon: 'ios-phone-portrait',
+            },
+            {
+                title: '   Sensor sensitivity',
+                backgroundColor: TEAL_BLUE,
+                icon: 'ios-radio',
+                
+            },
+        ],
+    },
+    {
+        data: [
+            {
+                title: '   Vibrate when name is spoken',
+                icon: 'ios-radio',
+                backgroundColor: GREEN,
+                rightTitle: 'Off'
+            },
+            {
+                title: '   Train with customized voices',
+                backgroundColor: GREY,
+                icon: 'ios-switch',
+            },
+            {
+                title: '   Do Not Disturb',
+                backgroundColor: PURPLE,
+                icon: 'ios-moon',
+            },
+        ],
+    },
+    {
+        data: [
+            {
+                title: '   General',
+                icon: 'ios-settings',
+                backgroundColor: GREY,
+            },
+            {
+                title: '   Display & Brightness',
+                backgroundColor: BLUE,
+                icon: 'ios-bulb',
+            },
+            
+            {
+                title: '   Sounds',
+                backgroundColor: RED,
+                icon: 'ios-volume-high',
+            },
+           
+            {
+                title: '   Confidentiality',
+                backgroundColor: GREY,
+                icon: 'ios-hand',
+            },
+        ],
+    },
+    // Space at the bottom
+    { data: [] },
 ];
 
+
 export default class Settings extends React.PureComponent {
-  renderItem = ({
-    item: {title, backgroundColor, icon, rightTitle, hideChevron, checkbox},
-  }) => (
-    <ListItem>
-      <ListItem.Content
-        style={{flexDirection: 'row', justifyContent: 'flex-end'}}>
-        <ListItem.Title
-          style={{
-            maarginStart: 10,
-          }}>
-          {title}
-        </ListItem.Title>
-        <Icon
-          name={icon}
-          size={30}
-          type="ionicon"
-          style={{
-            backgroundColor,
-            width: 40,
-            // height: 28,
-            borderRadius: 6,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        />
-      </ListItem.Content>
-      {!hideChevron && <ListItem.Chevron />}
-      {checkbox && <ListItem.CheckBox />}
-    </ListItem>
-    // <View >
+    renderItem = ({
+        item: { title, backgroundColor, icon, rightTitle, hideChevron, checkbox },
+        
+    }) => (
 
-    //   <Text>{title}</Text>
-    // </View>
-  );
+            <ListItem
+                containerStyle={{ paddingVertical: 8 }}
+                switch={checkbox && { value: true }}
+                key={title}
+                chevron={!hideChevron}
+                rightTitle={rightTitle}
+                leftIcon={{
+                    type: 'ionicon',
+                    name: icon,
+                    size: 20,
+                    color: 'white',
+                    containerStyle: {
+                        backgroundColor,
+                        width: 28,
+                        height: 28,
+                        borderRadius: 6,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    },
+                }}
+                title={title}
+            />
+            
+        );
 
-  renderSectionHeader = () => <View style={styles.headerSection} />;
+    renderSectionHeader = () => <View style={styles.headerSection} />;
 
-  ItemSeparatorComponent = () => (
-    <View style={styles.separatorComponent}>
-      <Divider style={styles.separator} />
-    </View>
-  );
 
-  ListHeaderComponent = () => (
-    <View>
-      <Header
-        statusBarProps={{barStyle: 'light-content'}}
-        barStyle="light-content"
-        leftComponent={
-          <Icon
-            name="menu"
-            onPress={() => this.props.navigation.openDrawer()}
-          />
-        }
-        centerComponent={
-          <Image
-            source={{
-              //uri: 'https://i.postimg.cc/dtCKLDbB/Hearme-Logo-Black.jpg'
-              uri: 'https://i.postimg.cc/SNCMqQP4/Logo-big.png',
-              // uri: 'https://i.postimg.cc/jdV3QB7F/IMG-20191105-10568.jpg'
-            }}
-            style={{
-              flex: 1,
-              height: '100%',
-              width: '100%',
-              resizeMode: 'contain',
-            }}
-          />
-        }
-        containerStyle={{
-          backgroundColor: '#3175b8',
-          justifyContent: 'space-around',
-          marginTop: 20,
-        }}
-      />
-      <Divider />
-    </View>
-  );
-
-  keyExtractor = (item, index) => index;
-
-  render() {
-    return (
-      <SectionList
-        keyExtractor={this.keyExtractor}
-        ListHeaderComponent={this.ListHeaderComponent}
-        contentContainerStyle={styles.containerStyle}
-        sections={sections}
-        renderItem={this.renderItem}
-        renderSectionHeader={this.renderSectionHeader}
-        ItemSeparatorComponent={this.ItemSeparatorComponent}
-        SectionSeparatorComponent={Divider}
-        stickySectionHeadersEnabled={false}
-      />
+    ItemSeparatorComponent = () => (
+        <View style={styles.separatorComponent}>
+            <Divider style={styles.separator} />
+        </View>
     );
-  }
+
+    ListHeaderComponent = () => (
+        <View>
+            <SearchBar platform="ios" placeholder="Search" />
+            <Divider />
+        </View>
+    );
+
+    keyExtractor = (item, index) => index;
+
+    render() {
+      
+        return (
+            <View>
+                <Header
+                    statusBarProps={{ barStyle: 'light-content' }}
+                    barStyle="light-content"
+                    leftComponent={<Icon name="menu" onPress={() => this.props.navigation.openDrawer()} />}
+                    centerComponent={{ text: 'Settings', style: { color: '#fff' } }}
+                    rightComponent={<Icon name= "settings"/>}
+                    containerStyle={{
+                        backgroundColor: '#3D6DCC',
+                        justifyContent: 'space-around',
+                    }}
+                />
+
+                <SectionList
+                    keyExtractor={this.keyExtractor}
+                    ListHeaderComponent={this.ListHeaderComponent}
+                    contentContainerStyle={styles.containerStyle}
+                    sections={sections}
+                    renderItem={this.renderItem}
+                    renderSectionHeader={this.renderSectionHeader}
+                    ItemSeparatorComponent={this.ItemSeparatorComponent}
+                    SectionSeparatorComponent={Divider}
+                    stickySectionHeadersEnabled={false}
+                />
+            </View>
+
+        );
+    }
 }
 
+
+
+
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#EFEFF4',
-  },
-  separatorComponent: {
-    backgroundColor: 'white',
-  },
-  separator: {
-    marginLeft: 58,
-  },
-  headerSection: {
-    height: 30,
-  },
+    container: {
+        backgroundColor: '#EFEFF4',
+    },
+    separatorComponent: {
+        backgroundColor: 'white',
+    },
+    separator: {
+        marginLeft: 58,
+    },
+    headerSection: {
+        height: 30,
+    },
 });
+
+
